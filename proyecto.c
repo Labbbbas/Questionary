1b20c97a0d20cf767821fd015b22ab4464da2692
Labbbbas <72531396+Labbbbas@users.noreply.github.com>
2020-12-28 21:45:29 -0600

    Main proyect

---
+++ proyecto.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int opcion,comprobar=0;
+
+    system("clear");
+    printf("\033[1;34m");
+    printf("\n\t\t\t\tBIENVENIDO A TRIVIARAMA");
+    printf("\033[1;33m");
+    printf("\n\n\n\t\t\t\t   ¿Que desea hacer?");
+    printf("\033[1;32m");
+    printf("\n\n\t\t\t\t1. Videojuegos");
+    printf("\033[1;32m");
+    printf("\n\t\t\t\t2. Musica");
+    printf("\033[1;32m");
+    printf("\n\t\t\t\t3. Peliculas");
+    printf("\033[1;32m");
+    printf("\n\t\t\t\t4. Matematicas");
+    printf("\033[1;32m");
+    printf("\n\t\t\t\t5. Cultura General");
+    printf("\033[1;32m");
+    printf("\n\t\t\t\t6. Salir");
+    printf("\033[1;36m");
+    printf("\n\n\n\t\t\t\tElija una opcion: ");
+    printf("\033[0m");
+    scanf("%d",&opcion);
+    system("clear");
+
+    switch (opcion){
+    case 1: printf("\033[1;32m");
+            printf("\n\n\t\t\t\t1. Videojuegos");
+            printf("\033[1;32m");
+            printf("");
+        break;
+    case 2: printf("\033[1;32m");
+            printf("\n\t\t\t\t2. Musica");
+            printf("\033[1;32m");
+            printf("");
+        break;
+    case 3: printf("\033[1;32m");
+            printf("\n\t\t\t\t3. Peliculas");
+            printf("\033[1;32m");
+            printf("");
+        break;
+    case 4: printf("\033[1;32m");
+            printf("\n\t\t\t\t4. Matematicas");
+            printf("\033[1;32m");
+            printf("");
+        break;
+    case 5: printf("\033[1;32m");
+            printf("\n\t\t\t\t5. Cultura General");
+            printf("\033[1;32m");
+            printf("");
+        break;
+    case 6: printf("\n\t\t\t\tVuelva pronto :)\n\n");
+        break;
+    default: printf("Opcion Invalida, intente de nuevo\n\n");
+        break;
+    }
+
+    return 0;
+}
+
+
+//      videojuegos
+// A quien se le conoce como el héroe del tiempo?
+// a) Link
+// b) Zelda
+// c) Jonh Wick
+// d) Toretto
+
+// Como se llama el hermano de Mario Bros?
+// a)	Luigi
+// b)	Browser
+// c)	Peach
+// d)	Toad
+
+// A cual se le conoce como al código konami?
+// a)	Arriba arriba abajo abajo izquierda derecha izquierda derecha A B Start
+// b)	Mantener presionado Start y Select al inicio del juego
+// c)	Golpear la consola
+// d)	Codigo fuente de los videojuegos
+
+// Que juego fue primero?
+// a)	Pong
+// b)	Pac-Man
+// c)	Sonic
+// d)	Mario Bros
+
+// Cual marca de videojuegos NO es real?
+// a)	Playstation
+// b)	Xbox
+// c)	DramCast
+// d)	KFConsole
+
+
+//      musica
+    // Cual es el disco mas vendido de la historia?
+    // a) De BTS
+    // b) Thriller de Michael Jackson
+    // c) De The Beatles
+    // d) The Rolling Stones
+
+    // cual fue el artista mas escuchado del 2020?
+    // a) Billie Eilish
+    // b) Bad Bunny beibe ye ye
+    // c) Camilo
+    // d) Christian Nodal
+
+    // Cual es la cancion con mas reproducciones en Youtube?
+    // a) Baby Shark
+    // b) Despacito
+    // c) Shape of You
+    // d) See you Again
+
+    // 
+
+
+//      peliculas
+¿Qué actor interpretó al mago Gandalf en la trilogía de El Señor de los anillos?
+a)Alfred Hitchcock.
+b)Tom Cruise
+c)Nathalie Portman 
+d)Ian Mckellen
+
+¿Por qué película ganó Leonardo DiCaprio su primer Óscar? 
+a) The Revenant
+b) The Wolf of Wall Street
+c) Catch me if you can
+d) The great Gatsby
+
+¿En qué película podemos encontrar al león Mufasa y a su hijo Simba?
+a) The Lion King
+b) Onward
+c) Bao
+d) Upside-Down Magic
+
+¿Quién interpreta a Freddie Mercury en la película Bohemian Rhapsody?
+a) Ben Hardy
+b) Rami Malek
+c) Tom Hanks
+d) Tobey Maguire
+
+¿Quién dirigió la película The Passion of the Christ? 
+a) Mel Gibson
+b) Quentin Tarantino
+c) Alfred Hitchcock
+d) Guillermo del Toro
+
+
+//      matematicas
+
+
+//      cultura general
+// quien es el creador del lenguaje C?
+//     a) Dennies Ritchie
+//     b) Steve Jobs
+//     c) Bill Gates
+//     d) Myke Towers
+
+// cuando finalizo la segunda guerra mundial?
+//     a) 1921
+//     b) 1945
+//     c) 1939
+//     d) 1917
+
+// cuanto duro la guerra de los 100 anos? 
+//     a) pos 100 anos
+//     b) 106 anos
+//     c) 99 anos 
+//     d) 116 anos 
+
+// cuando se descubrio america? 
+//     a) 1482
+//     b) 1492
+//     c) 1484
+//     d) 1494
+
+// que es un palindromo? 
+//     a) una palabra que se escribre diferente pero significa lo mismo
+//     b) una palabra que tiene un significado apuesto a otra palabra
+//     c) una palabra que se escribe igual al derecho y al reves
+//     d) un ave en peligro de extincion
